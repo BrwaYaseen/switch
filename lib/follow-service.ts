f77f1929eaa3d33a984da9a239c5f8e03d0c0b49
Brwa Yaseen <brwayasin29@gmail.com>
2024-08-23 15:12:23 +0300

    test

--- lib/follow-service.ts
+++ lib/follow-service.ts
@@ -142,7 +142,11 @@ export const getFollowedUsers = async () => {
         },
       },
       include: {
-        following: true,
+        following: {
+          include: {
+            stream: true,
+          },
+        },
       },
     });
 
