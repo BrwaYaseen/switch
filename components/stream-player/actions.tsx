00247f7049d85defdbbffd021497df778db8e0b4
Brwa Yaseen <brwayasin29@gmail.com>
2024-08-25 23:41:21 +0300

    Header

---
+++ components/stream-player/actions.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { useAuth } from "@clerk/nextjs";
+import { Button } from "../ui/button";
+import { Heart, HeartCrack } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import { onFollow, onUnFollow } from "@/actions/follow";
+import { toast } from "sonner";
+import { Skeleton } from "../ui/skeleton";
+
+type ActionsProps = {
+  isFollowing: boolean;
+  hostIdentity: string;
+  isHost: boolean;
+};
+
+export const Actions = ({
+  isFollowing,
+  hostIdentity,
+  isHost,
+}: ActionsProps) => {
+  const Icon = isFollowing ? HeartCrack : Heart;
+  const { userId } = useAuth();
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+
+  const handleFollow = () => {
+    startTransition(() => {
+      onFollow(hostIdentity)
+        .then((data) =>
+          toast.success(`You are now Following ${data.following.username}`)
+        )
+        .catch(() => toast.error("Something Went Wrong"));
+    });
+  };
+  const handleUnFollow = () => {
+    startTransition(() => {
+      onUnFollow(hostIdentity)
+        .then((data) =>
+          toast.success(`You have unfollowed ${data.following.username}`)
+        )
+        .catch(() => toast.error("Something Went Wrong"));
+    });
+  };
+
+  const toggleFollow = () => {
+    if (!userId) {
+      return router.push("/sign-in");
+    }
+
+    if (isHost) return;
+
+    if (isFollowing) {
+      handleUnFollow();
+    } else {
+      handleFollow();
+    }
+  };
+  return (
+    <Button
+      onClick={toggleFollow}
+      disabled={isPending || isHost}
+      variant="primary"
+      size="sm"
+      className="w-full lg:w-auto"
+    >
+      <Icon className="h-4 w-4 mr-2" />
+      {isFollowing ? "UnFollow" : "Follow"}
+    </Button>
+  );
+};
+
+export const ActionsSkeleton = () => {
+  return <Skeleton className="h-10 w-full lg:w-24" />;
+};
--- components/stream-player/stream-player.tsx
+++ components/stream-player/stream-player.tsx
@@ -8,6 +8,7 @@ import { useChatSidebar } from "@/store/use-chat-sidebar";
 import { cn } from "@/lib/utils";
 import { Chat, ChatSkeleton } from "./chat";
 import { ChatToggle } from "./chat-toggle";
+import { Header, HeaderSkeleton } from "./header";
 
 interface StreamPlayerProps {
   user: User & { stream: Stream | null };
@@ -47,6 +48,14 @@ export const StreamPlayer = ({
          2xl:col-span-5 lg:overflow-auto hidden-scrollbar pb-10"
         >
           <Video hostName={user.username} hostIdentity={user.id} />
+          <Header
+            hostName={user.username}
+            hostIdentity={user.id}
+            viewerIdentity={identity}
+            imageUrl={user.imageUrl}
+            isFollowing={isFollowing}
+            name={stream.name}
+          />
         </div>
         <div className={cn(" col-span-1", collapsed && "hidden")}>
           <Chat
@@ -72,6 +81,7 @@ export const StreamPlayerSkeleton = () => {
          2xl:col-span-5 lg:overflow-auto hidden-scrollbar pb-10"
       >
         <VideoSkeleton />
+        <HeaderSkeleton />
       </div>
       <div className=" col-span-1 bg-[#252731]">
         <ChatSkeleton />
