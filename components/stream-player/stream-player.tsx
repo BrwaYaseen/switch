f299e60bd1622fee5a4fc4ba13b2c4b5907b55a4
Brwa Yaseen <brwayasin29@gmail.com>
2024-08-27 19:17:51 +0300

    ending part 2

--- components/stream-player/stream-player.tsx
+++ components/stream-player/stream-player.tsx
@@ -11,10 +11,28 @@ import { ChatToggle } from "./chat-toggle";
 import { Header, HeaderSkeleton } from "./header";
 import { InfoCard } from "./info-card";
 import { AboutCard } from "./about-card";
+type CustomStream = {
+  id: string;
+  isLive: boolean;
+  isChatEnabled: boolean;
+  isChatDelayed: boolean;
+  isChatFollowersOnly: boolean;
+  thumbnailUrl: string | null;
+  name: string;
+};
+
+type CustomUser = {
+  id: string;
+  username: string;
+  bio: string | null;
+  imageUrl: string;
+  stream: CustomStream | null;
+  _count: { followedBy: number };
+};
 
 interface StreamPlayerProps {
-  user: User & { stream: Stream | null; _count: { followedBy: number } };
-  stream: Stream;
+  user: CustomUser;
+  stream: CustomStream;
   isFollowing: boolean;
 }
 
---
+++ lib/feed-service.ts
@@ -0,0 +1,70 @@
+import { getSelf } from "./auth-service";
+import { db } from "./db";
+
+export const getStreams = async () => {
+  let userId;
+
+  try {
+    const self = await getSelf();
+
+    if (!self) {
+      throw new Error("Unauthorized");
+    }
+    userId = self.id;
+  } catch {
+    userId = null;
+  }
+
+  let streams = [];
+
+  if (userId) {
+    streams = await db.stream.findMany({
+      where: {
+        user: {
+          NOT: {
+            blocking: {
+              some: {
+                blockedId: userId,
+              },
+            },
+          },
+        },
+      },
+      select: {
+        id: true,
+        user: true,
+        isLive: true,
+        name: true,
+        thumbnailUrl: true,
+      },
+      orderBy: [
+        {
+          isLive: "desc",
+        },
+        {
+          updatedAt: "desc",
+        },
+      ],
+    });
+  } else {
+    streams = await db.stream.findMany({
+      select: {
+        id: true,
+        user: true,
+        isLive: true,
+        name: true,
+        thumbnailUrl: true,
+      },
+      orderBy: [
+        {
+          isLive: "desc",
+        },
+        {
+          updatedAt: "desc",
+        },
+      ],
+    });
+  }
+
+  return streams;
+};
